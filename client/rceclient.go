05cf8119e363dd9dd77e0cc7073f99dfb8d3c751
Aman Prasad <prasdaman2000@gmail.com>
2026-09-20 01:03:41 -0700

    add ability to send/execute on/to multiple hosts

--- client/rceclient.go
+++ client/rceclient.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"pubsubclientlib"
 	"rcelib"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -17,8 +19,8 @@ func main() {
 		"--data_path defines the data path to transfer")
 	is_executable := flag.Bool("is_executable", false,
 		"--is_executable determines whether --data_path is an executable")
-	remote_alias := flag.String("remote_alias", "local",
-		"--remote_alias defines the host to send command to")
+	remote_aliases := flag.String("remote_aliases", "local",
+		"--remote_alias defines the hosts to send the command to (comma separated list)")
 	timeout := flag.Duration("timeout", 5*time.Second,
 		"--timeout defines how long to wait for the rce to complete")
 	out_file := flag.String("out_file", "/tmp/psrce_out",
@@ -44,61 +46,74 @@ func main() {
 		return
 	}
 
-	subscribeTopic := fmt.Sprintf("%s_Outputs", *remote_alias)
-	err = psclient.Subscribe(subscribeTopic)
-	if err != nil {
-		fmt.Printf("Subscribe error: %v\n", err)
-		return
-	}
+	var wg sync.WaitGroup
 
-	publishTopic := fmt.Sprintf("%s_Commands", *remote_alias)
-	payload, payloadID, err := rcelib.SerializePayload(filepath.Base(*data_path), *is_executable, fileBytes)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
-	}
-	err = psclient.Publish(publishTopic, payload)
-	if err != nil {
-		fmt.Printf("Publish error: %v\n", err)
-		return
-	}
+	funcToRun := func(remote_alias string) {
+		defer wg.Done()
+		fmt.Printf("running command on %s\n", remote_alias)
+		subscribeTopic := fmt.Sprintf("%s_Outputs", remote_alias)
+		err = psclient.Subscribe(subscribeTopic)
+		if err != nil {
+			fmt.Printf("Subscribe error: %v\n", err)
+			return
+		}
 
-	waitInterval := *timeout / 10
-	waitStart := time.Now()
-	for {
-		messages, err := psclient.Poll()
+		publishTopic := fmt.Sprintf("%s_Commands", remote_alias)
+		payload, payloadID, err := rcelib.SerializePayload(filepath.Base(*data_path), *is_executable, fileBytes)
 		if err != nil {
 			fmt.Printf("%s\n", err)
 			return
 		}
-		foundResponse := false
-		for _, message := range messages {
-			resp, err := rcelib.DeserializeResponse(message.Msg)
+		err = psclient.Publish(publishTopic, payload)
+		if err != nil {
+			fmt.Printf("Publish error: %v\n", err)
+			return
+		}
+
+		waitInterval := *timeout / 10
+		waitStart := time.Now()
+		for {
+			messages, err := psclient.Poll()
 			if err != nil {
-				fmt.Printf("%s", err)
+				fmt.Printf("%s\n", err)
 				return
 			}
-			if resp.ID == payloadID {
-				if resp.Status == rcelib.RCEStatusError {
-					fmt.Printf("Error")
-				}
-				err = os.WriteFile(*out_file, resp.Data, 0755)
+			foundResponse := false
+			for _, message := range messages {
+				resp, err := rcelib.DeserializeResponse(message.Msg)
 				if err != nil {
-					fmt.Printf("%s\n", err)
+					fmt.Printf("%s", err)
+					return
 				}
-				fmt.Printf("Wrote command output to %s \n", *out_file)
-				foundResponse = true
+				if resp.ID == payloadID {
+					if resp.Status == rcelib.RCEStatusError {
+						fmt.Printf("Error")
+					}
+					err = os.WriteFile(*out_file, resp.Data, 0755)
+					if err != nil {
+						fmt.Printf("%s\n", err)
+					}
+					fmt.Printf("Wrote command output to %s \n", *out_file)
+					foundResponse = true
+				}
 			}
+			if foundResponse {
+				break
+			}
+			timeSinceStart := time.Since(waitStart)
+			if timeSinceStart > *timeout {
+				fmt.Printf("Did not get a response in %s, exiting.\n", timeSinceStart)
+				break
+			}
+			fmt.Printf("Response not found, waiting for %s...\n", waitInterval)
+			time.Sleep(waitInterval)
 		}
-		if foundResponse {
-			break
-		}
-		timeSinceStart := time.Since(waitStart)
-		if timeSinceStart > *timeout {
-			fmt.Printf("Did not get a response in %s, exiting.\n", timeSinceStart)
-			break
-		}
-		fmt.Printf("Response not found, waiting for %s...\n", waitInterval)
-		time.Sleep(waitInterval)
 	}
+
+	for _, remote_alias := range strings.Split(*remote_aliases, ",") {
+		go funcToRun(remote_alias)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println("all workers finished")
 }
